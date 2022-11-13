b13e66e083ee88b513b259a0b70a7e9fdb5a16c1
Rūta Jankevičiūtė <jankevicruta@gmail.com>
2022-11-13 13:41:07 +0200

    More minor refactoring

--- helper.py
+++ helper.py
@@ -4,6 +4,10 @@ def get_parameter_value(x, w, b):
     v = x*w + b
     return v
 
+def get_layer2_parameter_value(y, w, b):
+    v2 = sum(np.multiply(y, w)) + b
+    return v2
+
 def desired_fromula(x):
     y = (1 + 0.6 * np.sin(2 * np.pi * x / 0.7)) + 0.3 * np.sin(2 * np.pi * x)/ 2
     return y
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import helper as h
 import matplotlib.pyplot as plt
-#np.random.seed(4)
+np.random.seed(4)
 d = []
 x = np.linspace(0.1, 1, 20)
 d = h.desired_fromula(x)
@@ -23,32 +23,20 @@ for i in range(10000):
     for j in range(len(x)):
         v1 = h.get_parameter_value(x[j],w1,b1)
         y1 = h.sigmoid(v1)
-        
-        v2 = sum(np.multiply(y1, w2)) + b2
-
+        v2 = h.get_layer2_parameter_value(y1, w2, b2)
         e = d[j] - v2
-
         delta = y1 * (1-y1) * (e * w2)
-        
         w2 += p * e * y1
         w1 += p * delta * x[j]
         b1 += p * delta
         b2 += p * e
-    
 
 d2 = []
 for i in range(len(x)):
     v1 = h.get_parameter_value(x[i],w1,b1)
     y1 = h.sigmoid(v1)
-    d2.append(sum(np.multiply(y1, w2)) + b2)
+    d2.append(h.get_layer2_parameter_value(y1, w2, b2))
 
-print('w1 ', w1)
-print('w2 ', w2)
-print('b2 ', b2)
-print('d ', d)
-print('d2 ', d2)
 fig, ax = plt.subplots()
-
 ax.plot(x, d, x, d2)
-
 plt.show()
